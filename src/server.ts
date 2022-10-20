858a107e5ed9e66e8e95a2c431c68d6a3d0c9f39
Renan Soares <renansoares@live.com>
2022-10-20 00:43:23 -0300

    init

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import { oi } from "../src/oi";
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const getPostTitles = async () => {
   try {
